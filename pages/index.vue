90032479730c6a07cac82dcf368313b26e73ea8b
Miles <milest94@gmail.com>
2018-10-22 13:44:15 +0200

    new vue project

---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <section class="container">
+    <div>
+      <h1 class="title">
+        Hello world
+      </h1>
+    </div>
+  </section>
+</template>
+
+
+<style>
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+</style>
+
